82f154055c6daa19152c36564b9a94c31530f3bf
marsprj <marsprj@gmail.com>
2014-10-09 15:23:44 +0800

    read local conf arguments from nginx.conf

--- ngx_http_auge_module/ngx_http_auge_module.c
+++ ngx_http_auge_module/ngx_http_auge_module.c
@@ -1,33 +1,16 @@
-#include <ngx_config.h>
-#include <ngx_core.h>
-#include <ngx_http.h>
-#include <string.h>
-
-#include "ngx_auge_database.h"
-
-PGconn *pgConn = NULL;
-
-
-static char* ngx_http_auge(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
-static void* ngx_http_auge_create_loc_conf(ngx_conf_t *cf);
-static char* ngx_http_auge_merge_loc_conf(ngx_conf_t *cf, void* parent, void *child);
-
-/* init auge process handler */
-static ngx_int_t ngx_http_auge_init_process(ngx_cycle_t *cycle);
-/* exit auge process handler */
-static void ngx_http_auge_exit_process(ngx_cycle_t *cycle);
+#include "ngx_http_auge_module.h"
 
 typedef struct{
-	ngx_str_t output_words;
+	ngx_str_t name;
 }ngx_http_auge_loc_conf_t;
 
 static ngx_command_t ngx_http_auge_commands[] = {
 	{
-		ngx_string("auge"),
-		NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
-		ngx_http_auge,
+		ngx_string("name"),
+		NGX_HTTP_LOC_CONF|NGX_CONF_NOARGS|NGX_CONF_TAKE1,
+		ngx_http_auge_name,
 		NGX_HTTP_LOC_CONF_OFFSET,
-		offsetof(ngx_http_auge_loc_conf_t, output_words),
+		offsetof(ngx_http_auge_loc_conf_t, name),
 		NULL
 	},
 
@@ -35,14 +18,14 @@ static ngx_command_t ngx_http_auge_commands[] = {
 };
 
 static ngx_http_module_t ngx_http_auge_module_ctx = {
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	ngx_http_auge_create_loc_conf,
-	ngx_http_auge_merge_loc_conf
+	NULL,					/* preconfiguration */
+	ngx_http_auge_init,			/* postconfiguration */
+	NULL,					/* create main configuration */
+	NULL,					/* init main configuration */
+	NULL,					/* create server configuration */
+	NULL,					/* merge server configuration */
+	ngx_http_auge_create_loc_conf,		/* create location configuration */
+	ngx_http_auge_merge_loc_conf		/* merge location configuration */
 };
 
 ngx_module_t ngx_http_auge_module = {
@@ -63,9 +46,7 @@ ngx_module_t ngx_http_auge_module = {
 static ngx_int_t
 ngx_http_auge_handler(ngx_http_request_t *r)
 {
-	return auge_db_query(pgConn, "cities", r);
-
-/*	ngx_int_t rc;
+	ngx_int_t rc;
 	ngx_buf_t *b;
 	ngx_chain_t out[2];
 
@@ -94,10 +75,23 @@ ngx_http_auge_handler(ngx_http_request_t *r)
 	}
 	
 	rc = ngx_http_output_filter(r, &out[0]);
-
-
 	return rc;
-*/
+}
+
+static ngx_int_t ngx_http_auge_init(ngx_conf_t *cf)
+{
+	ngx_http_handler_pt 		*h = NULL;
+	ngx_http_core_main_conf_t	*cmcf=NULL;
+
+	cmcf  = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+	h = ngx_array_push(&cmcf->phases[NGX_HTTP_CONTENT_PHASE].handlers);
+	if(h==NULL)
+	{
+		return NGX_ERROR;
+	}
+
+	*h = ngx_http_auge_handler;
+	return NGX_OK;
 }
 
 static void*
@@ -110,8 +104,8 @@ ngx_http_auge_create_loc_conf(ngx_conf_t *cf)
 	{
 		return NGX_CONF_ERROR;
 	}
-	conf->output_words.len = 0;
-	conf->output_words.data = NULL;
+	conf->name.len = 0;
+	conf->name.data = NULL;
 
 	return conf;
 }
@@ -122,31 +116,32 @@ ngx_http_auge_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
 	ngx_http_auge_loc_conf_t *prev = parent;
 	ngx_http_auge_loc_conf_t *conf = child;
 
-	ngx_conf_merge_str_value(conf->output_words, prev->output_words, "auge");
+	ngx_conf_merge_str_value(conf->name, prev->name, "auge");
 	
 	return NGX_CONF_OK;
 }
 
 static char* 
-ngx_http_auge(ngx_conf_t *cf, ngx_command_t *cmd, void* conf)
+ngx_http_auge_name(ngx_conf_t *cf, ngx_command_t *cmd, void* conf)
 {
 	ngx_http_core_loc_conf_t *clcf;
+	clcf  = conf;
+	char* rv = ngx_conf_set_str_slot(cf, cmd,conf);
+
+	ngx_conf_log_error(NGX_LOG_EMERG, cf, 0, "[Auge Name]:%s",clcf->name.data);
 	
-	clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+/*	clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
 	clcf->handler = ngx_http_auge_handler;
 	
 	ngx_conf_set_str_slot(cf, cmd, conf);
-	
-	return NGX_CONF_OK;
+*/	
+	return rv;
 }
 
 static ngx_int_t 
 ngx_http_auge_init_process(ngx_cycle_t *cycle)
 {
 	ngx_log_error(NGX_LOG_DEBUG, cycle->log, 0, "init auge process");
-
-	auge_db_connect(&pgConn, "127.0.0.1","5432","gisdb","postgres","qwer1234");
-
 	return 0;
 }
 
@@ -154,8 +149,6 @@ static void
 ngx_http_auge_exit_process(ngx_cycle_t *cycle)
 {
 	ngx_log_error(NGX_LOG_DEBUG, cycle->log, 0, "exit auge process");
-
-	auge_db_close(pgConn);
 }
 
 
