cb7393cc0b6c344daeb7ef73f600994b01f0bd67
marsprj <marsprj@gmail.com>
2015-05-27 15:40:52 +0800

    comment some method in AugeData.h file

--- include/AugeData.h
+++ include/AugeData.h
@@ -155,8 +155,8 @@ namespace auge
 
 		virtual RESULTCODE			Register(const char* name, const char* engine, const char* constr) = 0;
 		virtual RESULTCODE			Unregister(const char* name) = 0;
-		virtual RESULTCODE			Update(const char* name, const char* engine, const char* constr) = 0;
-		virtual RESULTCODE			Update(g_uint gid, const char* name, const char* engine, const char* constr) = 0;
+		//virtual RESULTCODE			Update(const char* name, const char* engine, const char* constr) = 0;
+		//virtual RESULTCODE			Update(g_uint gid, const char* name, const char* engine, const char* constr) = 0;
 
 		virtual bool				Has(const char* name) = 0;
 		virtual g_int				GetID(const char* name) = 0;
