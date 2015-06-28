b1eec92e492316f0edc5fb52461ae86a5071b4fc
marsprj <marsprj@gmail.com>
2015-06-28 17:23:51 +0800

    add aqi data

---
+++ AQI/aqi_area_update.sql
@@ -0,0 +1,1 @@
+update gc_aqi set position_name='睢阳区环保局' where area='商丘' and position_name = '睢阳区环保局　';
---
+++ AQI/aqi_ranking.sql
@@ -0,0 +1,173 @@
+update gc_aqi_city set name='哈密地区' where name = '哈密';
+update gc_aqi_city set name='楚雄州' where name = '楚雄';
+update gc_aqi_city set name='贵港市' where name = '贵港';
+update gc_aqi_city set name='德宏州' where name = '德宏傣族景颇族自治州';
+update gc_aqi_city set name='普洱' where name = '普洱哈尼族彝族自治县';
+update gc_aqi_city set name='巴中' where name = '巴中县';
+update gc_aqi_city set name='云浮' where name = '云浮县';
+update gc_aqi_city set name='迪庆州' where name = '迪庆藏族自治州';
+update gc_aqi_city set name='丽江' where name = '丽江纳西族自治县';
+update gc_aqi_city set name='昌都地区' where name = '昌都';
+update gc_aqi_city set name='伊犁哈萨克州' where name = '伊犁哈萨克自治州';
+update gc_aqi_city set name='延边州' where name = '延边朝鲜族自治州';
+update gc_aqi_city set name='平度' where name = '平度市';
+update gc_aqi_city set name='红河州' where name = '红河县';
+update gc_aqi_city set name='林芝地区' where name = '林芝';
+update gc_aqi_city set name='怒江州' where name = '怒江傈僳族自治州';
+update gc_aqi_city set name='松原' where name = '松原市';
+update gc_aqi_city set name='阿坝县' where name = '阿坝州';
+update gc_aqi_city set name='吐鲁番地区' where name = '吐鲁番';
+update gc_aqi_city set name='鄂尔多斯' where name = '鄂尔多斯市';
+update gc_aqi_city set name='海门' where name = '海门县';
+update gc_aqi_city set name='章丘' where name = '章丘县';
+update gc_aqi_city set name='塔城地区' where name = '塔城市';
+update gc_aqi_city set name='西双版纳州' where name = '西双版纳傣族自治州';
+update gc_aqi_city set name='池州' where name = '池州市';
+update gc_aqi_city set name='即墨' where name = '即墨市';
+update gc_aqi_city set name='石河子' where name = '石河子市';
+update gc_aqi_city set name='蓬莱' where name = '蓬莱县';
+update gc_aqi_city set name='昌吉州' where name = '昌吉';
+update gc_aqi_city set name='恩施州' where name = '恩施';
+update gc_aqi_city set name='台州' where name = '台州市';
+update gc_aqi_city set name='宜兴' where name = '宜兴市';
+update gc_aqi_city set name='文山州' where name = '文山县';
+update gc_aqi_city set name='日喀则地区' where name = '日喀则';
+update gc_aqi_city set name='兴安盟' where name = '兴安县';
+update gc_aqi_city set name='胶南' where name = '胶南市';
+update gc_aqi_city set name='文登' where name = '文登市';
+update gc_aqi_city set name='甘孜' where name = '甘孜县';
+update gc_aqi_city set name='昆山' where name = '昆山市';
+update gc_aqi_city set name='眉山' where name = '眉山县';
+update gc_aqi_city set name='莱西' where name = '莱西市';
+update gc_aqi_city set name='太仓' where name = '太仓县';
+update gc_aqi_city set name='玉树州' where name = '玉树';
+update gc_aqi_city set name='广安' where name = '广安县';
+update gc_aqi_city set name='吕梁' where name = '吕梁市';
+update gc_aqi_city set name='寿光' where name = '寿光县';
+update gc_aqi_city set name='莱州' where name = '莱州市';
+update gc_aqi_city set name='资阳' where name = '资阳县';
+update gc_aqi_city set name='大理州' where name = '大理';
+update gc_aqi_city set name='宿迁' where name = '宿迁市';
+update gc_aqi_city set name='呼伦贝尔' where name = '呼伦贝尔市';
+update gc_aqi_city set name='那曲地区' where name = '那曲';
+update gc_aqi_city set name='海西州' where name = '海西蒙古族藏族自治州';
+update gc_aqi_city set name='甘南州' where name = '甘南县';
+update gc_aqi_city set name='黔南州' where name = '黔南布衣族苗族自治州';
+update gc_aqi_city set name='张家界' where name = '张家界市';
+update gc_aqi_city set name='果洛州' where name = '果洛藏族自治州';
+update gc_aqi_city set name='黄南州' where name = '黄南藏族自治州';
+update gc_aqi_city set name='句容' where name = '句容县';
+update gc_aqi_city set name='凉山' where name = '凉山彝族自治州';
+update gc_aqi_city set name='吴江' where name = '吴江县';
+update gc_aqi_city set name='莱芜' where name = '莱芜市';
+update gc_aqi_city set name='胶州' where name = '胶州市';
+update gc_aqi_city set name='来宾' where name = '来宾县';
+update gc_aqi_city set name='宜城' where name = '宜城县';
+update gc_aqi_city set name='贺州' where name = '贺州市';
+update gc_aqi_city set name='白山' where name = '白山市';
+update gc_aqi_city set name='达州' where name = '达州市';
+update gc_aqi_city set name='乳山' where name = '乳山县';
+update gc_aqi_city set name='陇南' where name = '陇南市';
+update gc_aqi_city set name='泰州' where name = '泰州市';
+update gc_aqi_city set name='湘西州' where name = '湘西土家族苗族自治州';
+update gc_aqi_city set name='哈密地区' where name = '哈密';
+update gc_aqi_city set name='随州' where name = '随州市';
+update gc_aqi_city set name='襄阳' where name = '襄阳市';
+update gc_aqi_city set name='商洛' where name = '商洛市';
+
+update gc_aqi_city set name='中卫' where name = '中卫县';
+update gc_aqi_city set name='临夏州' where name = '临夏';
+update gc_aqi_city set name='临安' where name = '临安县';
+update gc_aqi_city set name='义乌' where name = '义乌市';
+update gc_aqi_city set name='乌兰察布' where name = '乌兰县';
+update gc_aqi_city set name='五家渠' where name = '五家渠市';
+update gc_aqi_city set name='亳州' where name = '亳州市';
+update gc_aqi_city set name='凉山州' where name = '凉山';
+update gc_aqi_city set name='博州' where name = '博尔塔拉蒙古自治州';
+update gc_aqi_city set name='和田地区' where name = '和田';
+update gc_aqi_city set name='喀什地区' where name = '喀什';
+update gc_aqi_city set name='富阳' where name = '富阳县';
+update gc_aqi_city set name='崇左' where name = '崇左县';
+update gc_aqi_city set name='常熟' where name = '常熟市';
+update gc_aqi_city set name='庆阳' where name = '庆阳县';
+update gc_aqi_city set name='张家港' where name = '张家港市';
+update gc_aqi_city set name='抚州' where name = '抚州市';
+update gc_aqi_city set name='招远' where name = '招远县';
+update gc_aqi_city set name='揭阳' where name = '揭阳县';
+update gc_aqi_city set name='晋中' where name = '晋中市';
+update gc_aqi_city set name='江阴' where name = '江阴市';
+update gc_aqi_city set name='海北州' where name = '海北藏族自治州';
+update gc_aqi_city set name='海南州' where name = '海南藏族自治州';
+update gc_aqi_city set name='淮安' where name = '淮安市';
+update gc_aqi_city set name='溧阳' where name = '溧阳县';
+update gc_aqi_city set name='潮州' where name = '潮州市';
+update gc_aqi_city set name='瓦房店' where name = '瓦房店市';
+update gc_aqi_city set name='甘孜州' where name = '甘孜';
+update gc_aqi_city set name='荆州' where name = '荆州市';
+update gc_aqi_city set name='荣成' where name = '荣成市';
+update gc_aqi_city set name='葫芦岛' where name = '葫芦岛市';
+update gc_aqi_city set name='贵港' where name = '贵港市';
+update gc_aqi_city set name='金坛' where name = '金坛县';
+update gc_aqi_city set name='铜仁地区' where name = '铜仁';
+update gc_aqi_city set name='阿克苏地区' where name = '阿克苏';
+update gc_aqi_city set name='阿坝州' where name = '阿坝县';
+update gc_aqi_city set name='阿拉善盟' where name = '阿拉善左旗';
+update gc_aqi_city set name='黄冈市' where name = '黄冈市';
+update gc_aqi_city set name='黔东南州' where name = '黔东南苗族侗族自治州';
+update gc_aqi_city set name='黔南州' where name = '黔南布依族苗族自治州';
+update gc_aqi_city set name='黔西南州' where name = '黔西县';
+
+
+
+
+宜城：112.256631 31.717172
+巴彦淖尔:107.408944 40.759381
+锡林郭勒盟：116.092468 43.98462
+黄冈：114.887445 30.457435
+防城港：108.362153 21.691939
+
+insert into gc_aqi_city (name,pyname,pos,type) values('宜城', 'Yicheng',st_geomfromtext('POINT(112.256631 31.717172)',4326),'county');
+insert into gc_aqi_city (name,pyname,pos,type) values('巴彦淖尔', 'Bayannaoer',st_geomfromtext('POINT(107.408944 40.759381)',4326),'city');
+insert into gc_aqi_city (name,pyname,pos,type) values('锡林郭勒盟', 'Xilinguole',st_geomfromtext('POINT(116.092468 43.98462)',4326),'city');
+insert into gc_aqi_city (name,pyname,pos,type) values('黄冈', 'Hanggang',st_geomfromtext('POINT(114.887445 30.457435)',4326),'city');
+insert into gc_aqi_city (name,pyname,pos,type) values('防城港', 'Fangchenggang',st_geomfromtext('POINT(108.362153 21.691939)',4326),'city');
+
+insert into gc_aqi_city (name,pyname,pos,type) values('宜城市', 'Yicheng',st_geomfromtext('POINT(112.256631 31.717172)',4326),'county');
+insert into gc_aqi_city (name,pyname,pos,type) values('巴彦淖尔', 'Bayannaoer',st_geomfromtext('POINT(107.408944 40.759381)',4326),'city');
+insert into gc_aqi_city (name,pyname,pos,type) values('锡林郭勒盟', 'Xilinguole',st_geomfromtext('POINT(116.092468 43.98462)',4326),'city');
+insert into gc_aqi_city (name,pyname,pos,type) values('黄冈市', 'Hanggang',st_geomfromtext('POINT(114.887445 30.457435)',4326),'city');
+insert into gc_aqi_city (name,pyname,pos,type) values('防城港市', 'Fangchenggang',st_geomfromtext('POINT(108.362153 21.691939)',4326),'city');
+
+
+CREATE TABLE gc_aqi_ranking
+(
+  id integer NOT NULL,
+  aqi integer,
+  area character varying(255),
+  co real,
+  co_24h real,
+  no2 integer,
+  no2_24h integer,
+  o3 integer,
+  o3_24h integer,
+  o3_8h integer,
+  o3_8h_24h integer,
+  pm10 integer,
+  pm10_24h integer,
+  pm2_5 integer,
+  pm2_5_24h integer,
+  quality character varying(255),
+  level character varying(255),
+  so2 integer,
+  so2_24h integer,
+  primary_pollutant character varying(255),
+  time_point timestamp without time zone,
+  x double precision,
+  y double precision, -- 
+  pos geometry(Point,4326),
+  CONSTRAINT gc_aqi_ranking_pkey PRIMARY KEY (id)
+)
+
+ALTER TABLE gc_aqi_ranking  ADD COLUMN x double precision;
+ALTER TABLE gc_aqi_ranking  ADD COLUMN y double precision;
+select addgeometrycolumn('gc_aqi_ranking','pos',4326,'POINT',2);
