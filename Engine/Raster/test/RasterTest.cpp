a6b09f90f78d77f02361d1ce3f729552f0852e16
marsprj <marsprj@gmail.com>
2015-07-06 14:31:36 +0800

    add Raster test project

---
+++ Engine/Raster/test/RasterTest.cpp
@@ -0,0 +1,119 @@
+#include "RasterTest.h"
+#include "AugeCore.h"
+#include "AugeData.h"
+#include "AugeCarto.h"
+#include "AugeGeometry.h"
+#include "AugeProcessor.h"
+#include <iostream>
+
+CPPUNIT_TEST_SUITE_REGISTRATION(RasterTest);
+
+void RasterTest::setUp() 
+{
+	auge::GLogger	*pLogger = auge::augeGetLoggerInstance();
+	pLogger->Initialize();
+
+	auge::DataEngine	*pEngine = NULL;
+	auge::DataEngineManager* pEngineManager = NULL;
+	pEngineManager = auge::augeGetDataEngineManagerInstance();
+	pEngineManager->Load();
+
+	pEngine = pEngineManager->GetEngine("Postgres");
+	m_pConnection = pEngine->CreateConnection();
+	m_pConnection->SetConnectionString("SERVER=127.0.0.1;INSTANCE=5432;DATABASE=auge;USER=postgres;PASSWORD=qwer1234;ENCODING=GBK");
+	m_pConnection->Open();
+
+	auge::ConnectionManager* pConnManager = NULL;
+	pConnManager = auge::augeGetConnectionManagerInstance();
+	pConnManager->Initialize(m_pConnection);
+
+	printf("setUp\n");
+}
+
+void RasterTest::tearDown()
+{
+	auge::ConnectionManager* pConnManager = NULL;
+	pConnManager = auge::augeGetConnectionManagerInstance();
+	pConnManager->Unload();
+
+	auge::DataEngineManager* pEngineManager = NULL;
+	pEngineManager = auge::augeGetDataEngineManagerInstance();
+	pEngineManager->Unload();
+
+	//m_pConnection->Close();
+	//m_pConnection->Release();
+	m_pConnection = NULL;
+	printf("tearDown\n");
+}
+
+void RasterTest::ExtractByRectangle()
+{	
+	DWORD ts = GetTickCount();
+
+	auge::RasterExtractByRectangleProcessor* pProcessor = NULL;
+	auge::GProcessorFactory* pFactory = auge::augeGetGeoProcessorFactoryInstance();
+	pProcessor = pFactory->CreateRasterExtractByRectangleProcessor();
+
+	pProcessor->SetInputDataSource("rsdb");
+	pProcessor->SetInputRaster("Koala.png");
+
+	pProcessor->SetOutputDataSource("rsdb");
+	pProcessor->SetOutputRaster("Koala_clipped.png");
+
+	auge::GEnvelope extent(100,100,500,500);
+	pProcessor->SetInputRactangle(extent);
+
+	RESULTCODE rc = pProcessor->Execute();
+
+	pProcessor->Release();
+	
+	DWORD te = GetTickCount();
+	printf("[Ê±¼ä]:%dºÁÃë\n", te-ts);
+}
+
+void RasterTest::Read_png()
+{
+	const char* path = "E:\\Research\\Auge.GIS\\Dist\\32_x86_win_vc10\\binD\\upload\\Koala.png";
+
+	auge::Raster* pRaster = NULL;
+	auge::RasterIO* io = auge::augeGetRasterIOInstance();
+	pRaster = io->Read(path);
+
+	pRaster->Release();
+}
+
+void RasterTest::Read_tif()
+{
+	const char* path = "E:\\Research\\Auge.GIS\\Dist\\32_x86_win_vc10\\binD\\upload\\world.tif";
+
+	auge::Raster* pRaster = NULL;
+	auge::RasterIO* io = auge::augeGetRasterIOInstance();
+	pRaster = io->Read(path);
+
+	pRaster->Release();
+}
+
+void RasterTest::Create_Raster()
+{
+	const char* o_name = "world_new.tif";
+
+	const char* i_path = "E:\\Research\\Auge.GIS\\Dist\\32_x86_win_vc10\\binD\\upload\\world.tif";
+	const char* o_path = "E:\\Research\\Auge.GIS\\Dist\\32_x86_win_vc10\\binD\\upload\\world_new.tif";
+
+	//const char* i_path = "E:\\Research\\Auge.GIS\\Dist\\32_x86_win_vc10\\binD\\upload\\Koala.png";
+	//const char* o_path = "E:\\Research\\Auge.GIS\\Dist\\32_x86_win_vc10\\binD\\upload\\Koala_new.png";
+
+	//const char* i_path = "E:\\Research\\Auge.GIS\\Dist\\32_x86_win_vc10\\binD\\upload\\Koala.jpeg";
+	//const char* o_path = "E:\\Research\\Auge.GIS\\Dist\\32_x86_win_vc10\\binD\\upload\\Koala_new.jpeg";
+
+	auge::Raster* pinRaster = NULL;
+	auge::Raster* poutRaster = NULL;
+	auge::RasterIO* io = auge::augeGetRasterIOInstance();
+	pinRaster = io->Read(i_path);
+
+	auge::GEnvelope extent(0,0,90,90);
+	auge::RasterFactory* pRasterFactory = auge::augeGetRasterFactoryInstance();
+	poutRaster = pRasterFactory->CreateRaster(o_name, extent, pinRaster);
+
+	pinRaster->Release();
+}
---
+++ Engine/Raster/test/RasterTest.h
@@ -0,0 +1,31 @@
+#ifndef __AUGE_RASTER_TEST__H__
+#define __AUGE_RASTER_TEST__H__
+
+#include "cppunit/extensions/HelperMacros.h" 
+#include "AugeTile.h"
+
+class RasterTest : public CppUnit::TestFixture 
+{
+	CPPUNIT_TEST_SUITE(RasterTest);
+	//CPPUNIT_TEST(Read_png);
+	//CPPUNIT_TEST(Read_tif);
+	CPPUNIT_TEST(Create_Raster);
+	//CPPUNIT_TEST(ExtractByRectangle);
+	CPPUNIT_TEST_SUITE_END();
+public:
+	void setUp();
+	void tearDown();
+
+private:
+	void	ExtractByRectangle();
+
+	void	Read_png();
+	void	Read_tif();
+
+	void	Create_Raster();
+
+private:
+	auge::GConnection	*m_pConnection;
+};
+
+#endif //__AUGE_RASTER_TEST__H__
