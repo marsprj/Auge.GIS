71ac68842fe5548c670f9e44bf51e44a4813d9fa
marsprj <marsprj@gmail.com>
2015-08-17 20:14:18 +0800

    gsims GetSymbol modify Region to Fill

--- Engine/Server/gsims/src/GetSymbolHandler.cpp
+++ Engine/Server/gsims/src/GetSymbolHandler.cpp
@@ -98,7 +98,7 @@ namespace auge
 			else
 				pSymbol = pSymbolManager->CreateLineSymbol(name);
 		}
-		else if(g_stricmp(type, "Region")==0)
+		else if(g_stricmp(type, "Fill")==0)
 		{
 			if(name==NULL)
 				pSymbols = pSymbolManager->GetFillSymbols();
