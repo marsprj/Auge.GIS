e9e523da2ef1d1092ba1d50fc7810cccb7484fd2
marsprj <marsprj@gmail.com>
2015-08-24 09:08:06 +0800

    test

--- Engine/Server/gsgps/src/CapabilitiesHandler.cpp
+++ Engine/Server/gsgps/src/CapabilitiesHandler.cpp
@@ -228,12 +228,19 @@ namespace auge
 	{
 		XElement* pxOperationsMetadata = pxParent->AddChild("OperationsMetadata", "ows");
 
-		AddOperationsNodes_1_1_0(pxOperationsMetadata, m_pEngine->m_feature_handlers, "Feature", "要素分析", gps_xlink);
-		AddOperationsNodes_1_1_0(pxOperationsMetadata, m_pEngine->m_raster_handlers, "Raster", "栅格分析", gps_xlink);
-		AddOperationsNodes_1_1_0(pxOperationsMetadata, m_pEngine->m_dem_handlers, "DEM", "地形分析", gps_xlink);
-		AddOperationsNodes_1_1_0(pxOperationsMetadata, m_pEngine->m_geometry_handlers, "Geometry", "空间分析", gps_xlink);
-		AddOperationsNodes_1_1_0(pxOperationsMetadata, m_pEngine->m_tile_handlers, "Tile", "瓦片金字塔", gps_xlink);
-		AddOperationsNodes_1_1_0(pxOperationsMetadata, m_pEngine->m_cluster_handlers, "Cluster", "聚类", gps_xlink);
+		//AddOperationsNodes_1_1_0(pxOperationsMetadata, m_pEngine->m_feature_handlers, "Feature", "要素分析", gps_xlink);
+		//AddOperationsNodes_1_1_0(pxOperationsMetadata, m_pEngine->m_raster_handlers, "Raster", "栅格分析", gps_xlink);
+		//AddOperationsNodes_1_1_0(pxOperationsMetadata, m_pEngine->m_dem_handlers, "DEM", "地形分析", gps_xlink);
+		//AddOperationsNodes_1_1_0(pxOperationsMetadata, m_pEngine->m_geometry_handlers, "Geometry", "空间分析", gps_xlink);
+		//AddOperationsNodes_1_1_0(pxOperationsMetadata, m_pEngine->m_tile_handlers, "Tile", "瓦片金字塔", gps_xlink);
+		//AddOperationsNodes_1_1_0(pxOperationsMetadata, m_pEngine->m_cluster_handlers, "Cluster", "聚类", gps_xlink);
+
+		AddOperationsNodes_1_1_0(pxOperationsMetadata, m_pEngine->m_feature_handlers, "Feature", "", gps_xlink);
+		AddOperationsNodes_1_1_0(pxOperationsMetadata, m_pEngine->m_raster_handlers, "Raster", "", gps_xlink);
+		AddOperationsNodes_1_1_0(pxOperationsMetadata, m_pEngine->m_dem_handlers, "DEM", "", gps_xlink);
+		AddOperationsNodes_1_1_0(pxOperationsMetadata, m_pEngine->m_geometry_handlers, "Geometry", "", gps_xlink);
+		AddOperationsNodes_1_1_0(pxOperationsMetadata, m_pEngine->m_tile_handlers, "Tile", "", gps_xlink);
+		AddOperationsNodes_1_1_0(pxOperationsMetadata, m_pEngine->m_cluster_handlers, "Cluster", "", gps_xlink);
 	}
 
 	void GeoProcessingCapabilitiesHandler::AddOperationsNodes_1_1_0(XElement* pxParent, std::vector<WebHandler*>& handlers, const char* type, const char* description, const char* gps_xlink)
