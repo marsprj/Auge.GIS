edc67531042ce7811eadd9dc62f3e568a91a3969
marsprj <marsprj@gmail.com>
2015-09-01 22:54:24 +0800

    add EnumJobImpl_2.cpp

---
+++ Engine/Server/gscore/src/EnumJobImpl_2.cpp
@@ -0,0 +1,46 @@
+#include "EnumJobImpl.h"
+
+namespace auge
+{
+	EnumJobImpl::EnumJobImpl()
+	{
+		m_iter = m_jobs.begin();
+	}
+
+	EnumJobImpl::~EnumJobImpl()
+	{
+		Cleanup();
+	}
+
+	Job* EnumJobImpl::Next()
+	{
+		if(m_iter==m_jobs.end())
+		{
+			return NULL;
+		}
+		return *(m_iter++);
+	}
+
+	void EnumJobImpl::Add(Job* pJob)
+	{
+		if(pJob!=NULL)
+		{
+			m_jobs.push_back(pJob);
+		}
+	}
+
+	void EnumJobImpl::Cleanup()
+	{
+		Job* pJob = NULL;
+		std::vector<Job*>::iterator iter;
+		for(iter=m_jobs.begin(); iter!=m_jobs.end(); iter++)
+		{
+			pJob = *iter;
+			if(pJob!=NULL)
+			{
+				pJob->Release();
+			}
+		}
+		m_jobs.clear();
+	}
+}
