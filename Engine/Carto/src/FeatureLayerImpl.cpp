15e26f4d12b2fdafd5a48219cc8bce41a50983ae
marsprj <marsprj@gmail.com>
2015-09-09 15:46:59 +0800

    bugfix : FeatureLayer::GetExtent return invalid value

--- Engine/Carto/src/FeatureLayerImpl.cpp
+++ Engine/Carto/src/FeatureLayerImpl.cpp
@@ -98,6 +98,10 @@ namespace auge
 	
 	GEnvelope& FeatureLayerImpl::GetExtent()
 	{
+		if(m_pFeatureClass==NULL)
+		{
+			m_pFeatureClass = GetFeatureClass();
+		}
 		if(m_pFeatureClass!=NULL)
 		{
 			m_extent = m_pFeatureClass->GetExtent();
