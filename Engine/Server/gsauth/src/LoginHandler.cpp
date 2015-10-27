ad09dac05af80497cf5ddda0df19d20890423d10
marsprj <marsprj@gmail.com>
2015-10-27 14:22:48 +0800

    create user folder when user login

--- Engine/Server/gsauth/src/LoginHandler.cpp
+++ Engine/Server/gsauth/src/LoginHandler.cpp
@@ -142,6 +142,105 @@ namespace auge
 
 	WebResponse* LoginHandler::Execute(WebRequest* pWebRequest, WebContext* pWebContext, User* pUser)
 	{
-		return Execute(pWebRequest, pUser);	
+		GLogger* pLogger = augeGetLoggerInstance();
+		LoginRequest* pRequest = static_cast<LoginRequest*>(pWebRequest);
+		const char* name = pRequest->GetName();
+		const char* pswd = pRequest->GetPassword();
+		const char* remote = pRequest->GetRemoteAddress();
+
+		if(name==NULL)
+		{
+			const char* msg = "Parameter [Name] is NULL";
+			WebExceptionResponse* pExpResponse = augeCreateWebExceptionResponse();
+			pExpResponse->SetMessage(msg);
+			pLogger->Error(msg, __FILE__, __LINE__);
+			return pExpResponse;
+		}
+		if(pswd==name)
+		{
+			const char* msg = "Parameter [Name] is NULL";
+			WebExceptionResponse* pExpResponse = augeCreateWebExceptionResponse();
+			pExpResponse->SetMessage(msg);
+			pLogger->Error(msg, __FILE__, __LINE__);
+			return pExpResponse;
+		}
+
+		User* pdbUser = NULL;
+		UserManager* pUserManager = augeGetUserManagerInstance();
+		pdbUser = pUserManager->GetUser(name);
+		if(pdbUser==NULL)
+		{
+			char msg[AUGE_MSG_MAX];
+			g_sprintf(msg, "用户[%s]不存在", name);
+			GError* pError = augeGetErrorInstance();
+			pError->SetError(msg);
+			WebExceptionResponse* pExpResponse = augeCreateWebExceptionResponse();
+			pExpResponse->SetMessage(msg);
+			pLogger->Error(msg, __FILE__, __LINE__);
+			return pExpResponse;
+		}
+
+		if(pdbUser->GetPassword()==NULL)
+		{	
+			char msg[AUGE_MSG_MAX];
+			g_sprintf(msg, "用户[%s]密码错误", name);
+			GError* pError = augeGetErrorInstance();
+			pError->SetError(msg);
+			WebExceptionResponse* pExpResponse = augeCreateWebExceptionResponse();
+			pExpResponse->SetMessage(msg);
+			pLogger->Error(msg, __FILE__, __LINE__);
+			pdbUser->Release();
+			return pExpResponse;
+		}
+		pdbUser->Release();
+
+		//if(strcmp(pdbUser->GetPassword(), pswd))
+		//{
+		//	const char* msg = "密码错误";
+		//	GError* pError = augeGetErrorInstance();
+		//	pError->SetError(msg);
+		//	WebExceptionResponse* pExpResponse = augeCreateWebExceptionResponse();
+		//	pExpResponse->SetMessage(msg);
+		//	pLogger->Error(msg, __FILE__, __LINE__);
+		//	pdbUser->Release();
+		//	return pExpResponse;
+		//}
+		RESULTCODE rc = pUserManager->Login(name, pswd, remote);
+		if(rc!=AG_SUCCESS)
+		{
+			GError* pError = augeGetErrorInstance();
+			WebExceptionResponse* pExpResponse = augeCreateWebExceptionResponse();
+			pExpResponse->SetMessage(pError->GetLastError());
+			pLogger->Error(pError->GetLastError(), __FILE__, __LINE__);
+			return pExpResponse;
+		}
+
+		char user_root[AUGE_PATH_MAX];
+		memset(user_root, 0, AUGE_PATH_MAX);
+		auge_make_path(user_root, NULL, pWebContext->GetUserRoot(), name, NULL);
+		if(g_access(user_root, 4))
+		{
+			auge_mkdir(user_root);
+		}
+
+		char user_file_root[AUGE_PATH_MAX];
+		memset(user_file_root, 0, AUGE_PATH_MAX);
+		auge_make_path(user_file_root, NULL, user_root, "upload", NULL);
+		if(g_access(user_file_root, 4))
+		{
+			auge_mkdir(user_file_root);
+		}
+
+		char user_raster_root[AUGE_PATH_MAX];
+		memset(user_raster_root, 0, AUGE_PATH_MAX);
+		auge_make_path(user_raster_root, NULL, user_root, "rds", NULL);
+		if(g_access(user_raster_root, 4))
+		{
+			auge_mkdir(user_raster_root);
+		}
+
+		WebSuccessResponse* pSusResponse = augeCreateWebSuccessResponse();
+		pSusResponse->SetRequest(pRequest->GetRequest());
+		return pSusResponse;
 	}
 }
